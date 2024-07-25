7703ae8fc9c213f0171eef1eaca21e153bc8a963
ONEIROS <xon3iros@gmail.com>
2024-07-25 19:49:42 +0330

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Display the header
+clear
+cat << "EOF"
+ ____ ____ ____ ____ ____ ____ ____
+||O |||N |||E |||I |||R |||O |||S ||
+||__|||__|||__|||__|||__|||__|||__||
+|/__\|/__\|/__\|/__\|/__\|/__\|/__\|
+EOF
+echo "https://x.com/0xOneiros"
+sleep 4
+
+# Function to return to the main menu
+return_to_menu() {
+    read -p "Press Enter to return to the main menu..."
+    exec "$0"
+}
+
+# Display the menu
+PS3='Please enter your choice: '
+options=("Allora_Node_TestNet" "Chasm_Network" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Allora_Node_TestNet")
+            # Download and execute script for option 1
+            curl -O https://example.com/Allora_Node_TestNet.sh
+            chmod +x Allora_Node_TestNet.sh
+            ./Allora_Node_TestNet.sh
+            return_to_menu
+            break
+            ;;
+        "Chasm_Network")
+            # Download and execute script for option 2
+            curl -O https://example.com/Chasm_Network.sh
+            chmod +x Chasm_Network.sh
+            ./Chasm_Network.sh
+            return_to_menu
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
